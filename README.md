6fe2208287c7eb9bd32a3c055d2da02968cd8069
philbranding <philbranding@gmail.com>
2020-07-07 23:42:21 +0200

    intial setup

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# zimstore
+# Go to C: >> XAMPP >> htdocs >> and create the main folder for the project called "ecommerce", inside that create following folders for this project:
+
+1) admin_area
+
+2) customer
+
+3) functions
+
+4) images
+
+5) includes
+
+6) js
+
+7) styles
+
+The above are the main folders for the project which must be created inside the "ecommerce" folder.
+
+Inside the admin_area folder, create following sub-folders:
+
+1) includes
+
+2) product_images
+
+3) images
+
+4) styles
+
+Inside the customer folder, create the following sub-folders:
+
+1) customer_images
+
+2) functions
+
+3) images
+
+4) includes
+
+5) styles
+
---
+++ index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<!-- Designed and developed by Phillip Mudavanhu https://thezimstore.com/ -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="the Zim Store offers a convenient way for Zimbabweans in the diaspora to buy and send groceries to thier loved one back home">
+    <meta name="Phil" content="the">
+    <title>Home · The Zim Store</title>
+
+    <!-- Bootstrap core CSS -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="./css/jquery.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="./css/popper.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="./css/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="./css/font-awesome.min.css">
+
+    <!-- Custom styles for this template -->
+    <link href="./css/main.css" rel="stylesheet">
+
+</head>
+
+
+<body data-gr-c-s-loaded="true">
+
+    <!-- Header -->
+    <header class="width-max:">
+        
+        <!-- Fixed NavBar -->
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+
+            <a class="nav-link " href="https://thezimstore.com/">
+                <img class="logo nav-item" src="./img/zimstore-o-logo.png" alt="logo" height="70px" width="70px">
+            </a>
+            <a class="navbar-brand h1" href="https://thezimstore.com/">the zimstore | </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="https://thezimstore.com/#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://thezimstore.com/cart.html">All Products</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://thezimstore.com/cart.html">My Account</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://thezimstore.com/cart.html">Sign Up</a>
+                    </li>
+                   <li class="nav-item">
+                        <a class="nav-link" href="https://thezimstore.com/cart.html">Contacts</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://thezimstore.com/cart.html">Shopping Cart</a>
+                    </li>
+                </ul>
+                <form class="form-inline mt-2 mt-md-0">
+                    <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+                 <a class="btn btn-success btn-lg ml-3" href="https://thezimstore.com/cart.html">
+                    <i class="fa fa-shopping-cart"></i> Cart
+                    <span class="badge badge-light">2</span>
+                </a>
+            </div>
+        </nav>
+        
+         <!-- End fixed NavBar-->
+    </header>
+ <!--End of Header -->
+
+
+    <div class="form-signin text-center" action="mailto:thezimstore@gmail.com" method="post" enctype="text/plain">
+        <img class="logo mb-4" src="./img/zimstore-logo.png" alt="" width="200px" height="200px">
+        <h1 class="h1 mb-3 font-weight-bold">Welcome to the ZimStore</h1>
+        <h1 class="h3 mb-4 font-weight-normal">Our website is still under construction, please Email: </h1>
+        <div class="h3">
+        <a href="mailto:thezimstore@gmail.com?subject=%20Enquiry%20on%20Buying%20and%20Sending%20Groceries%20to%20Zim"> thezimstore@gmail.com</a>
+        </div>
+    </div>
+
+    <footer>
+        <div class="container pt-5 border-bottom">
+            <div class="row">
+                <div class="col-md-3 col-sm-12 mb-3 text-center">
+                    <a class="nav-link " href="https://thezimstore.com/">
+                        <img class="logo nav-item" src="./img/zimstore-o-logo.png" alt="logo" height="70px" width="70px">
+                    </a>
+                </div>
+                <div class="col-md-9 col-sm-12">
+
+                    <div class="col-md-3 col-sm-6 col-6 p-0 float-left mb-3">
+                        <h5 class="mb-4 font-weight-bold text-uppercase">Our Services</h5>
+                        <ul class="list-group">
+                            <li class="list-group-item bg-transparent border-0 p-0 mb-2"><a href="https://thezimstore.com/#">Groceries Ordering</a></li>
+                            <li class="list-group-item bg-transparent border-0 p-0 mb-2"><a href="https://thezimstore.com/#">Groceries Delivery</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-md-3 col-sm-6 col-6 p-0 mb-3 float-left">
+                        <h5 class="mb-4 font-weight-bold text-uppercase">About Us</h5>
+                        <ul class="list-group">
+                            <li class="list-group-item bg-transparent border-0 p-0 mb-2"><a href="https://thezimstore.com/#">Our Team</a></li>
+                            <li class="list-group-item bg-transparent border-0 p-0 mb-2"><a href="https://thezimstore.com/#">Our Suppliers</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-md-3 col-sm-6 col-6 mb-3 p-0 float-left">
+                        <h5 class="mb-4 font-weight-bold text-uppercase">Company</h5>
+                        <ul class="list-group">
+                            <li class="list-group-item bg-transparent border-0 p-0 mb-2"><a href="https://thezimstore.com/#">About</a></li>
+                            <li class="list-group-item bg-transparent border-0 p-0 mb-2"><a href="https://thezimstore.com/#">Testimonies</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-md-3 col-sm-6 col-6 mb-3 p-0 float-left">
+                        <h5 class="mb-4 font-weight-bold text-uppercase">Connect</h5>
+                        <ul class="list-group">
+                            <li class="list-group-item bg-transparent border-0 p-0 mb-2">
+                                <a href="https://www.instagram.com/thezimstore/" target="_blank"><i class="fa fa-instagram mr-1"></i> Instagram</a>
+                            </li>
+                            <li class="list-group-item bg-transparent border-0 p-0 mb-2">
+                                <a href="https://thezimstore.com/#"><i class="fa fa-twitter mr-1"></i> Twitter</a>
+                                
+                            </li>
+                            <li class="list-group-item bg-transparent border-0 p-0 mb-2">
+                                <a href="https://thezimstore.com/#"><i class="fa fa-reddit mr-1"></i> Reddit</a>
+                            </li>
+                            <li class="list-group-item bg-transparent border-0 p-0 mb-2">
+                                <a href="https://thezimstore.com/#" target="_blank"><i class="fa fa-google-plus mr-1"></i> Google+</a>
+
+                            </li>
+                            <li class="list-group-item bg-transparent border-0 p-0 mb-2">
+                                <a href="https://thezimstore.com/#" target="_blank"><i class="fa fa-github mr-1"></i> Github</a>
+                            </li>
+                            <li class="list-group-item bg-transparent border-0 p-0 mb-2">
+                                <a href="https://thezimstore.com/#" target="_blank"><i class="fa fa-medium mr-1"></i> Medium</a>
+                            </li>
+                            <li class="list-group-item bg-transparent border-0 p-0 mb-2">
+                                <a href="https://thezimstore.com/#" target="_blank"><i class="fa fa-facebook mr-1"></i> Facebook</a>
+                            </li>
+                            <li class="list-group-item bg-transparent border-0 p-0 mb-2">
+                                <a href="https://thezimstore.com/#" target="_blank"><i class="fa fa-youtube mr-1"></i> YouTube</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+                <div class="col-md-12">
+                    <div class="py-4 d-flex justify-content-center align-items-center">
+                        <a class="mr-4" href="https://thezimstore.com/#">Privacy &amp; Terms</a>
+                        <a href="https://philbranding.com/" target="_blank">Designed and Developed by PhilBranding</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+
+
+</body></html>
