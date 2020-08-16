3437741413b33fcdf81d204ae6b0e420b099680e
philbranding <philbranding@gmail.com>
2020-08-16 22:50:27 +0200

    added the view and dynamic calls for the checkout.html  in the view.py

--- zimstore/zimstore/README.md
+++ zimstore/zimstore/README.md
@@ -257,3 +257,16 @@
         - ` total = sum([item.get_quantity for item in orderitems]) # `
         - ` return total `
         
+
+31. Add the view.py for the Checkout 
+        - ` def checkout(request): `
+        - ` if request.user.is_authenticated: `
+        - ` customer = request.user.customer `
+        - ` order, created = Order.objects.get_or_create(customer=customer, complete=False) `
+        - ` items = order.orderitem_set.all() `
+        - ` else: `
+        - ` # Create Empty cart for now for none-logged in users `
+        - ` order = {'get_cart_total': 0, 'get_cart_items': 0} `
+        - ` items = [] `
+        - ` context = {'items': items, 'order': order} `
+        - ` return render(request, 'store/checkout.html', context) `
--- zimstore/zimstore/store/views.py
+++ zimstore/zimstore/store/views.py
@@ -6,23 +6,32 @@ from .models import *
 
 def store(request):
     products = Product.objects.all()
-    context = {'products':products}
+    context = {'products': products}
     return render(request, 'store/store.html', context)
 
 
 def cart(request):
-
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()  # querying all the child orders that had this parent
     else:
         items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
 
     context = {'items': items, 'order': order}
     return render(request, 'store/cart.html', context)
 
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        # Create Empty cart for now for none-logged in users
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        items = []
+
+    context = {'items': items, 'order': order}
     return render(request, 'store/checkout.html', context)
